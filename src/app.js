5752b175d1e09665ed7cb439dd9b7a98981bdece
Thomas Rainero <thomas.rainero@TRUEBLUE.Local>
2019-04-09 15:57:07 +0200

    bugfix description

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
